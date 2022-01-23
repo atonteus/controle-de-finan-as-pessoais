449b2f07ac6227d5d638f67cdc499021045f34d0
Antonio Mateus <94945184+atonteus@users.noreply.github.com>
2022-01-22 23:49:30 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const transactionRouter = express.Router();
 
+const mongoose = require('mongoose');
 const service = require('../services/transactionService');
 
 
